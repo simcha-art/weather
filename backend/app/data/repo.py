643062c6cf4b0a573a87eb8befc431d75b72dec7
simcha <simchacop@gmail.com>
2026-09-15 09:57:50 +0300

    feat: add explorer name to favorites, so any explorer has his own favorites cities

--- backend/app/data/repo.py
+++ backend/app/data/repo.py
@@ -1,37 +1,45 @@
 from data.io import read_from_json, write_to_json
 
-def add_city(city: dict):
+def add_city(city: dict, explorer_name: str):
     try:
         cities = read_from_json()
-        cities.append(city)
+        
+        cities.get(explorer_name, []).append(city)
         write_to_json(cities)
         return True
     except Exception as e:
         print(e)
         return False
 
-def remove_city(city: dict):
+def remove_city(id: int, explorer_name: str):
     try:
         cities = read_from_json()
-        cities.remove(city)
+
+        explorer_cities: list = cities.get(explorer_name, [])
+        for i in range(len(explorer_cities)):
+            city = explorer_cities[i]
+            if city["id"] == id:
+                explorer_cities.pop(i)
+                break
+
         write_to_json(cities)
         return True
     except ValueError as e: 
         print(e)
         return False
 
-def get_all_cities():
+def get_all_cities(explorer_name: str):
     try:
-        return read_from_json()
+        return read_from_json()[explorer_name]
     except Exception as e:
         print(e)
 
-def get_by_id(id: int):
+def get_by_id(id: int, explorer_name: str):
     try: 
-        cities = read_from_json()
-        cities_ids = list(map(lambda c: c["id"] ,cities))
+        explorer_cities = read_from_json()[explorer_name]
+        cities_ids = list(map(lambda c: c["id"] ,explorer_cities))
         index = cities_ids.index(id)
-        return cities[index]
+        return explorer_cities[index]
     except Exception as e:
         print(e)
 
--- backend/app/routers/favoritesRouter.py
+++ backend/app/routers/favoritesRouter.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Path
 from data import repo
 from pydantic import BaseModel, Field
 from typing import Annotated
@@ -12,37 +12,38 @@ class City(BaseModel):
     longitude: Longitude
     timezone: str = Field(min_length=2)
 
-Id = Annotated[int, Query(gt=0)]
+Id = Annotated[int, Path(gt=0)]
+ExplorerName = Annotated[str, Path(min_length=2)]
 
 
 router = APIRouter(prefix="/favorites", tags=["favorites"])
 
-@router.post("/new", status_code=201)
-def create_new_city(city: City):
-    success = repo.add_city(city)
+@router.post("/{explorer_name}", status_code=201)
+def create_new_city(city: City, explorer_name: ExplorerName):
+    success = repo.add_city(city, explorer_name)
     if success:
         return {"success": success, "msg": "city added"}
     else: 
         raise HTTPException(500, "Internal server error")
 
-@router.delete("/delete")
-def delete_city(city: City):
-    success = repo.remove_city(city)
+@router.delete("/{explorer_name}/{city_id}")
+def delete_city(city_id: Id, explorer_name: ExplorerName):
+    success = repo.remove_city(city_id, explorer_name)
     if (success):
         return {"success": success, "msg": "city deleted"}
     else: 
-        raise HTTPException(404, f"city {city["id"]} not found")
+        raise HTTPException(404, f"city {city_id} not found")
 
-@router.get("")
-def get_all_favorites():
-    cities = repo.get_all_cities()
+@router.get("/{explorer_name}")
+def get_all_favorites(explorer_name: ExplorerName):
+    cities = repo.get_all_cities(explorer_name)
     return cities
 
-@router.get("/:id")
-def get_city_by_id(id: Id):
-    city = repo.get_by_id(id)
+@router.get("/{explorer_name}/{city_id}")
+def get_city_by_id(explorer_name: ExplorerName, city_id: Id ):
+    city = repo.get_by_id(city_id, explorer_name)
     if not city:
-        raise HTTPException(404, f"city {id} not found")
+        raise HTTPException(404, f"city {city_id} not found")
     else:
          return city
 
